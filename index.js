ccec7333ffc8ddf02d52caaaa3a85c124a723262
Khamal Ismadie <37945835+khamalismadie@users.noreply.github.com>
2022-05-28 20:29:20 +0700

    koreksi komentar

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ function lakukanLooping(arrPegawai) {
       Apabila imbang, maka komentar akan berisi:
         "Jumlah Pria dan Wanita berimbang"
   */
-  let komentar = [];
+  let komentar = "";
   if (jumlahPria > jumlahWanita) {
     komentar = "Jumlah Pria lebih banyak dari Wanita";
   } else if (jumlahWanita > jumlahPria) {
